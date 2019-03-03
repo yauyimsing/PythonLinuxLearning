6309a57ea5ab098ab2d3ef1208c9ae9dc647f774
yau linux <yauyimsing@gmail.com>
2019-03-03 22:11:12 +0800

    some learning change from linux project

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
+#-*- coding:utf-8 -*-
 # this is for test of learning python linux
-print('hello python linux')
 
+
+def func():
+    """this is a function used for test"""
+    print("hello world")
+    """printing hello world"""
+help(func)
